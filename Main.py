2b1e03106a93203658bd34b3696bbe6ac399d3e3
MoonRock <Bhattaarav2011@gmail.com>
2026-04-12 11:36:42 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 # This is the main source code file for the app-(name not finalised)
-import google.generativeai as genai
+import google.generativeai as gemini
 import os
 
 # Configure with your API key
-genai.configure(api_key="AIzaSyA_ENCdLto3XHB3fLHTGKF51DsjpSdBFuw")
+gemini.configure(api_key="AIzaSyA_ENCdLto3XHB3fLHTGKF51DsjpSdBFuw")
 
 # Initialize the model
-model = genai.GenerativeModel('gemini-2.5-flash')
+model = gemini.GenerativeModel('gemini-2.5-flash')
 
 # Generate text
 question=input('What do You Want To Ask: ')
 response = model.generate_content(question)
-print(response.text)
+print('Ans\n',response.text)
