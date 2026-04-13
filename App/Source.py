50da2a0dd8472393b2e4ec7c6094ccda2babfc37
MoonRock <Bhattaarav2011@gmail.com>
2026-04-13 22:33:05 +0530

    basic Source organization

--- App/Source.py
+++ App/Source.py
@@ -1,14 +1,71 @@
 # This is the main source code file for the app-(name not finalised)
-import google.generativeai as gemini
+
+
+# packages and modules
+import google.genai as genai
 import os
 
+
+
+
+# gemini (image recognistion)
+
+
+def analyis_image():
+    pass
 # Configure with your API key
-gemini.configure(api_key="AIzaSyA_ENCdLto3XHB3fLHTGKF51DsjpSdBFuw")
+gemini=genai.Client(api_key="AIzaSyA_ENCdLto3XHB3fLHTGKF51DsjpSdBFuw")
 
 # Initialize the model
-model = gemini.GenerativeModel('gemini-2.5-flash')
 
-# Generate text
-question=input('What do You Want To Ask: ')
-response = model.generate_content(question)
-print('Ans\n',response.text)
+model=gemini.models.generate_content(
+
+    model="gemini-2.5-pro",
+    contents='generate me a image of a elephant'
+)
+print(model.text)
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+#image Capturing/uploading
+# capturing
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+# Timer
+import timer as ti
+class clock():# to create different clocks
+    def __init__(self,time,increament):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------------------------------------------------------------------------
+# Stock Fish Analyis----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------------------------
