2b32846a3e9af396d20bf84bbdd563bc8923a1c5
MoonRock <Bhattaarav2011@gmail.com>
2026-05-11 22:25:14 +0530

    testing

--- App/experiment.py
+++ App/experiment.py
@@ -1,15 +1,17 @@
 
-import numpy as np
 
 
 
-from paddleocr import PaddleOCR
+
+from paddleocr import TextRecognition
 image_path=r"App\Data\temporary_data.png"
-ocr = PaddleOCR(use_doc_orientation_classify=False, 
-    use_doc_unwarping=False, 
-    use_textline_orientation=False,lang='en'
-    )
-result=ocr.ocr(image_path)
+
+
+model = TextRecognition()
+output = model.predict(input=image_path)
+
+print(output)
+    
 # def text_detection(image_path:str,image_bgr:np.ndarray) -> np.ndarray:
 #     engine=ocr.TextDetection()
 #     result=engine.predict(image_path if image_bgr is None else image_bgr)
