7c6de0c026896058bae5e6e45ee5084f96312817
MoonRock <Bhattaarav2011@gmail.com>
2026-06-26 15:34:39 +0530

    Implement audio recording functionality in voice_control.cleanup of data files and organization for image and aduio data format

--- paddleocr2/voice_controlled.py
+++ paddleocr2/voice_controlled.py
@@ -17,8 +17,29 @@ pipline for audical_scoresheet:
 """
 # imports(primary)
 from whisper import Whisper
-
+import sounddevice as sd
 #imports(secondary)
 """pending"""
+import numpy
+from scipy.io.wavfile import write
 #--------------------------------------------------------------------------------------------------------------------------------
-class 
+# part1: recording the move(this must happen via the front hand or backend i am at doubt)
+class recorder:
+    def __init__(self):
+        self.sample_rate = 16000
+        sd.default.samplerate =self.sample_rate
+        
+    def record(self,duration = 10 ):
+        """this function is to record the move spoken by the user when,the user presses his clock after playing on hi move"""
+        print('recording ')
+        audio = sd.rec(
+                int(duration * self.sample_rate),
+                samplerate=self.sample_rate,
+                channels=1
+            )
+        sd.wait()
+        write(r"App\temp_data\images\audio_files\recording.wav", self.sample_rate, audio)
+        print("Saved!")
+
+rec = recorder
+rec.record()
