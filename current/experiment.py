0f61997900918d8d18971d7d52eadd76d262a25b
Vihaan-Vasudeva <128611847+Vihaan-Vasudeva@users.noreply.github.com>
2026-08-24 09:08:12 +0530

    Delete current directory

--- current/experiment.py
+++
@@ -1,9 +0,0 @@
-
-import whisper
-
-model = whisper.load_model('small.en')
-
-# print('started')
-result = model.transcribe(r'App\Data\audio_files\test9.wav',fp16=False)
-print(result['text'])
-# print('completed')
